e8db6e9a84b68e2934b52f6d18c7bfdf5df0efe9
Raphael Carbonnel <rcarbonn@c1r6p1.42lehavre.fr>
2023-11-21 21:08:11 +0100

    bfdbjhfbdabsjk

--- ft_utils2.c
+++ ft_utils2.c
@@ -1,41 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_parse_args_quoted.c                             :+:      :+:    :+:   */
+/*   ft_utils2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcarbonn <rcarbonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/21 01:46:25 by rcarbonn          #+#    #+#             */
-/*   Updated: 2023/11/21 18:52:24 by rcarbonn         ###   ########.fr       */
+/*   Created: 2023/11/21 19:02:19 by rcarbonn          #+#    #+#             */
+/*   Updated: 2023/11/21 19:02:31 by rcarbonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_freestr(char **lst)
-{
-	char	*t1;
-
-	if(!lst)
-		return;
-	while(*lst)
-	{
-		t1 = *lst;
-		lst++;
-		free(t1);
-	}
-	*lst = NULL;
-}
-
-t_stack *ft_parse_args_quoted(char **argv)
-{
-	t_stack	*stack_a;
-	char	**tmp;
-	
-	stack_a = NULL;
-	tmp = ft_split(argv[1], 32);
-	list_args(tmp, &stack_a);
-	ft_free(tmp);
-	free(tmp);
-	return(stack_a);
-}
